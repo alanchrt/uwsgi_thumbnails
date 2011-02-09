da25f243d1478c375f3b720abbee0964d19d6959
Alan Christopher Thomas <alan.christopher.thomas@gmail.com>
2011-02-09 02:20:05 +0000

    made object-oriented and removed settings.py dependency

--- generate.py
+++ generate.py
@@ -18,70 +18,81 @@ Components:
 
 import hmac
 from PIL import Image
-from settings import DEBUG, SECRET_KEY, IMAGE_ROOT, THUMB_ROOT
 
-def application(environ, start_response):
-    try:
-        # Grab the request path
-        request_uri = environ['REQUEST_URI']
+class ThumbnailGenerator(object):
+    def __init__(self, secret_key=None, image_root=None, thumb_root=None,
+                 debug=False):
+        # Save attributes
+        self.secret_key = secret_key
+        self.image_root = image_root
+        self.thumb_root = thumb_root
+        self.debug = debug
 
-        # Check for root url
-        if request_uri == '/':
-            start_response('403 Forbidden', [('Content-Type', 'text/plain')])
-            yield "Forbidden"
-            return
+    def application(self, environ, start_response):
+        """Parses the URI and performs the desired transform."""
+        try:
+            # Grab the request path
+            request_uri = environ['REQUEST_URI']
 
-        # Break apart uri for parsing
-        path = request_uri.split('?')
-        file_data = path[0].split('.')
-        filename = file_data[0][1:]
-        parameters = filename.split('_')
-        dimensions = parameters[2].split('x')
+            # Check for root url
+            if request_uri == '/':
+                start_response('403 Forbidden',
+                               [('Content-Type','text/plain')])
+                yield "Forbidden"
+                return
 
-        # Store image metadata
-        image = {
-            'id': parameters[0],
-            'hash': parameters[1],
-            'width': dimensions[0],
-            'height': dimensions[1],
-            'transform': parameters[3],
-            'extension': file_data[1],
-            'signature': path[1],
-        }
+            # Break apart uri for parsing
+            path = request_uri.split('?')
+            file_data = path[0].split('.')
+            filename = file_data[0][1:]
+            parameters = filename.split('_')
+            dimensions = parameters[2].split('x')
 
-        # Validate transform type (only scaling for now)
-        if not image['transform'] in ('s',):
-            raise Exception, "Invalid image transform"
+            # Store image metadata
+            image = {
+                'id': parameters[0],
+                'hash': parameters[1],
+                'width': dimensions[0],
+                'height': dimensions[1],
+                'transform': parameters[3],
+                'extension': file_data[1],
+                'signature': path[1],
+            }
 
-        # Determine the required signature
-        h = hmac.new(SECRET_KEY)
-        h.update(image['id'])
-        h.update(image['hash'])
-        h.update(image['width'])
-        h.update(image['height'])
-        h.update(image['transform'])
-        h.update(image['extension'])
-        signature = h.hexdigest()[:24]
+            # Validate transform type (only scaling for now)
+            if not image['transform'] in ('s',):
+                raise Exception, "Invalid image transform"
 
-        # Validate image signature
-        if not image['signature'] == signature:
-            raise Exception, "Invalid signature '%s'" % signature
+            # Determine the required signature
+            h = hmac.new(self.secret_key)
+            h.update(image['id'])
+            h.update(image['hash'])
+            h.update(image['width'])
+            h.update(image['height'])
+            h.update(image['transform'])
+            h.update(image['extension'])
+            signature = h.hexdigest()[:24]
 
-        # Create the thumbnail
-        im = Image.open('%s%s_%s.%s' % (IMAGE_ROOT, image['id'],
-                                        image['hash'], image['extension'])) 
-        size = (int(image['width']), int(image['height']))
-        im.thumbnail(size, Image.ANTIALIAS)
-        im.save('%s%s.%s' % (THUMB_ROOT, filename, image['extension']))
+            # Validate image signature
+            if not image['signature'] == signature:
+                raise Exception, "Invalid signature '%s'" % signature
 
-        # Redirect to the newly created file
-        start_response('302 Found', [('Location', request_uri)])
+            # Create the thumbnail
+            im = Image.open('%s%s_%s.%s' % (self.image_root, image['id'],
+                                            image['hash'], image['extension']))
+            size = (int(image['width']), int(image['height']))
+            im.thumbnail(size, Image.ANTIALIAS)
+            im.save('%s%s.%s' % (self.thumb_root, filename,
+                                 image['extension']))
 
-    except Exception, error:
-        # Display "File not found" error
-        start_response('404 Not Found', [('Content-Type', 'text/plain')])
-        if DEBUG:
-            # Output extra 512 bytes to override Chrome friendly 404
-            yield "File not found: %s%s" % (error, ("\n" + " "*32)*16)
-        else:
-            yield "File not found"
+            # Redirect to the newly created file
+            start_response('302 Found', [('Location', request_uri)])
+
+        except Exception, error:
+            # Display "File not found" error
+            start_response('404 Not Found', [('Content-Type', 'text/plain')])
+            if self.debug:
+                # Output extra 512 bytes to override Chrome friendly 404
+                yield "File not found: %s%s" % (error, ("\n" + " "*32)*16)
+            else:
+                yield "File not found"
